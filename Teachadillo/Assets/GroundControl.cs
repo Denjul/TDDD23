de444b0bdb392368b46a8205c1e7f7921147d292
khenrix <chrhe230@student.liu.se>
2015-09-17 16:33:16 +0200

    Kitten that can jump and move

---
+++ Teachadillo/Assets/GroundControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundControl : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update () {
+		float speed = .5f;
+		float offset = Time.time * speed;                             
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, -offset); 
+	}
+}
---
+++ Teachadillo/Assets/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	CharacterController controller;
+	bool isGrounded= false;
+	public float vert = Input.GetAxis("Vertical")+10;
+	public float speed = 6.0f;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20.0f;
+	private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller.isGrounded) {
+			GetComponent<Animation>().Play("Run");
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), vert, 0);  //get keyboard input to move in the horizontal direction
+			moveDirection = transform.TransformDirection(moveDirection);  //apply this direction to the character
+			moveDirection *= speed;            //increase the speed of the movement by the factor "speed" 
+		}
+
+		if (Input.GetButton ("Jump")) {
+			GetComponent<Animation>().Stop("Run");
+			GetComponent<Animation>().Play("Jump");
+			moveDirection.y = jumpSpeed;
+		}
+		if (controller.isGrounded) {
+			isGrounded = true;
+		}
+		moveDirection.y -= gravity * Time.deltaTime;       //Apply gravity  
+		controller.Move(moveDirection * Time.deltaTime); 
+	}
+}
