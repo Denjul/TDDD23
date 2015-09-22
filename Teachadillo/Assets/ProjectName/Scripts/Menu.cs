e53ff7848e566a0cef8fba2523d4136ed5de0f1a
Dennis Julin <Denniskajulin@gmail.com>
2015-09-23 01:00:33 +0200

    Added start menu scean

---
+++ Teachadillo/Assets/ProjectName/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+		public GUISkin myskin;
+		public string gameLevel;
+		public string optionsLevel;
+		
+		private void OnGUI()
+		{
+			GUI.skin=myskin;
+			
+			if (GUI.Button(new Rect(Screen.width/4+10, Screen.height/4+Screen.height/10+10, Screen.width/2-20, Screen.height/10), "PLAY")){
+				Application.LoadLevel(gameLevel);
+			}
+			if (GUI.Button(new Rect(Screen.width/4+10, Screen.height/4+2*Screen.height/10+10, Screen.width/2-20, Screen.height/10), "EXIT")){
+				Application.Quit();	
+			}
+		}
+	}
+
